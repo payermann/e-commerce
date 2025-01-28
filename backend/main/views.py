83ce5c91bce928ce562545a2034ccae14019a4fc
Felix <payermann@gmail.com>
2025-01-28 11:45:46 +0100

    Add links and views for products and actions

--- backend/main/views.py
+++ backend/main/views.py
@@ -11,3 +11,17 @@ class IndexView(generic.ListView):
         context = super().get_context_data(**kwargs)
         context['actions'] = Action.objects.all()
         return context
+
+class ProductView(generic.DetailView):
+    model = Product
+    template_name = 'product.html'
+
+class ActionView(generic.DetailView):
+    model = Action
+    template_name = 'action.html'
+
+class ProductListView(generic.ListView):
+    model = Product
+    template_name = 'product_list.html'
+    paginate_by = 30
+    context_object_name = 'products'
